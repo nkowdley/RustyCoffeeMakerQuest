d15f6540844f16a83b7838af0a03774371eed5fa
Neel Kowdley <nkowdley@gmail.com>
2016-04-13 21:40:52 -0400

    add a test case for the constructor

--- src/player.rs
+++ src/player.rs
@@ -2,9 +2,18 @@
 fn it_works() {
     assert_eq!(4, 4);
 }
-fn can_win() {
-	p.get_cream();
-	p.get_coffee();
-	p.get_sugar();
-	assert!(p.can_win());
+
+#[test]
+#[should_panic(expected = "assertion failed")] /*Mark that this test case should fail*/
+fn test_constructor() {
+ let p = Player::new();
+ assert!(p.has_cream);
+ assert!(p.has_coffee);
+ assert!(p.has_sugar);
 }
+// fn can_win() {
+// 	p.get_cream();
+// 	p.get_coffee();
+// 	p.get_sugar();
+// 	assert!(p.can_win());
+// }
