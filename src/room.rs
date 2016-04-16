6b47bf479fd42fb7f2770b6a87733976d813636a
Neel Kowdley <nkowdley@gmail.com>
2016-04-16 04:15:34 -0400

    failing test for room 0 constructor

--- src/room.rs
+++ src/room.rs
@@ -6,16 +6,30 @@
 */
 /*initialize the room struct*/
 struct Room {
-
+	has_north_door: bool ,
+	has_south_door: bool,
+	description: String,
+	adj:String
 }
 /*Room methods*/
 #[allow(dead_code)]
 impl Room {
+	/*Constructor*/
+	pub fn new(room_num: i32) -> Room {
+		Room {
+			has_north_door: false,
+			has_south_door: false,
+			description: "".to_string(),
+			adj:"".to_string()
+		}
+	}
 
 }
 /*Test Cases*/
-/*Initial Test Case to prove that Cargo Test works*/
-// #[test]
-// fn it_works() {
-//
-// }
+/*Verify that the starting room(index 0) only has a north door*/
+#[test]
+fn room_0_north_door() {
+	/*Allocate a new room*/
+	let r = Room::new(0);
+	assert!(r.has_north_door,true);
+}
