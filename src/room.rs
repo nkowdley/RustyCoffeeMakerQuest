eab77b555ae8cbf9f26af555119a61057fa60714
Neel Kowdley <nkowdley@gmail.com>
2016-04-16 04:28:23 -0400

    create similar tests for south door

--- src/room.rs
+++ src/room.rs
@@ -84,24 +84,61 @@ fn room_1_north_door() {
 #[test]
 fn room_2_north_door() {
 	/*Allocate a new room*/
-	let r = Room::new(1);
+	let r = Room::new(2);
 	assert!(r.has_north_door,true);
 }
 #[test]
 fn room_3_north_door() {
 	/*Allocate a new room*/
-	let r = Room::new(1);
+	let r = Room::new(3);
 	assert!(r.has_north_door,true);
 }
 #[test]
 fn room_4_north_door() {
 	/*Allocate a new room*/
-	let r = Room::new(1);
+	let r = Room::new(4);
 	assert!(r.has_north_door,true);
 }
 #[test]
 fn room_5_north_door() {
 	/*Allocate a new room*/
-	let r = Room::new(1);
+	let r = Room::new(5);
 	assert!(r.has_north_door,false);
+}
+/*Test whether each room has the correct status for the south door*/
+#[test]
+fn room_0_south_door() {
+	/*Allocate a new room*/
+	let r = Room::new(0);
+	assert!(r.has_south_door,true);
+}
+#[test]
+fn room_1_south_door() {
+	/*Allocate a new room*/
+	let r = Room::new(1);
+	assert!(r.has_south_door,true);
+}
+#[test]
+fn room_2_south_door() {
+	/*Allocate a new room*/
+	let r = Room::new(2);
+	assert!(r.has_south_door,true);
+}
+#[test]
+fn room_3_south_door() {
+	/*Allocate a new room*/
+	let r = Room::new(3);
+	assert!(r.has_south_door,true);
+}
+#[test]
+fn room_4_south_door() {
+	/*Allocate a new room*/
+	let r = Room::new(4);
+	assert!(r.has_south_door,true);
+}
+#[test]
+fn room_5_south_door() {
+	/*Allocate a new room*/
+	let r = Room::new(5);
+	assert!(r.has_south_door,false);
 }
