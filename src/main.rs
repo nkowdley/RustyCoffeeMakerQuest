2c521b5aa1f506cf06e97e14bdb5c5bfb1239856
Neel Kowdley <nkowdley@gmail.com>
2016-04-16 19:09:23 -0400

    add template for main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod player;
 mod room;
 
+#[allow(dead_code)]
 fn main() {
 	/*Print the welcome message*/
 	println!("Coffee Maker Quest v2.0: The Rusty Coffee Maker");
@@ -9,5 +10,15 @@ fn main() {
 	println!("The goal of the game is to collect sugar, coffee, and cream so that you can study.");
 	/*initialize the array of rooms*/
 	let house : [room::Room; 6] = [ room::Room::new(0), room::Room::new(1), room::Room::new(2), room::Room::new(3), room::Room::new(4), room::Room::new(5) ];
+	/*intialize a player*/
+	let mut p = player::Player::new();
+	/*set a boolean to stop the game*/
+	let run : bool = true;
+	while (run)
+	{
+		//prompt_user_input();
+		//if prompt_user_input:
+		//do things
+	}
 
 }
