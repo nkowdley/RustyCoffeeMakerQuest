5328e7dc2cd98fcee9ddf3fe87ad1e8e9f1603a2
dms163 <dms163@pitt.edu>
2016-04-19 05:24:04 -0400

    near final commit I think

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
 
 use std::io;
 use std::io::prelude::*;
-use std::ops::Add::*;
 use std::string::String;
-use std::convert::AsRef;
-
 use player::{Player};
 pub mod player;
 use room::{Room};
 pub mod room;
 
-static mut current_room: usize = 0;
-
 #[allow(dead_code)]
 fn main() {
+
 	/*Print the welcome message*/
+	println!(" ");
 	println!("Coffee Maker Quest v2.0: The Rusty Coffee Maker");
 	println!("Instructions for Coffee Maker Quest - ");
 	println!("You are a brave student trying to study for the final exam in Bill Laboon's Software Testing Class, but you need caffeine.");
 	println!("The goal of the game is to collect sugar, coffee, and cream so that you can study.");
+	
 	/*initialize the array of rooms*/
 	let house : [room::Room; 6] = [ room::Room::new(0), room::Room::new(1), room::Room::new(2), room::Room::new(3), room::Room::new(4), room::Room::new(5) ];
 	/*intialize a player*/
@@ -28,15 +26,15 @@ fn main() {
 	let mut index: usize = 0;
 	while run==0
 	{
+		/*print out the room description.*/
 		println!("you see a {} room.  You find {}.", house[index as usize].get_adj(), house[index as usize].get_description() );
+		/*Create and get the input*/
 		let mut input:String = get_user_input();
-		//Loop until valid command has been given.
-		println!("here is input: {}",input);
-
-		while !validate_user_input(&input) {
+		/*Loop until valid command has been given.*/
+		while !validate_user_input(& input) {
 			println!("Invalid Command!");
 			input = get_user_input();
-		}
+		}/*Execute the command set the while condition to the result of execute_command*/
 		run = execute_command(&input, & house, &mut p, &mut index );
 	}
 }
@@ -48,45 +46,36 @@ fn main() {
 * Returns the users input to be validated.
 */
 fn get_user_input() -> String {
-	println!(" INSTRUCTIONS (N,S,L,I,D) > ");
+	println!("INSTRUCTIONS (N,S,L,I,H,D) > ");
 	println!("Please enter a command!");
 	let mut input = String::new();
     let stdin = io::stdin();
     stdin.lock().read_line(&mut input).unwrap();
-    input.trim().to_string()
+    input.trim().to_uppercase().to_string()
 }
 /*
-* validate_user_input(input: &String)
+* validate_user_input(input: &str)
 * Validates the users input and converts to uppercase.
 * Returns true if the input is valid else returns false.
 */
-fn validate_user_input(input:&str) -> bool {
-	let mut upper_input = String::new();
-	let n = "N".to_string();
-	let s = "S".to_string();
-	let l = "L".to_string();
-	let i = "I".to_string();
-	let h = "H".to_string();
-	let d = "D".to_string();
-	upper_input = input.to_uppercase();
-	upper_input = upper_input.to_string();
+fn validate_user_input(input:& str) -> bool {
 
-	if upper_input == n{
+	if input == "N"{
 		true
 	}
-	else if upper_input == s {
+	else if input == "S" {
 		true
 	}
-	else if upper_input == l {
+	else if input == "L" {
 		true
 	}
-	else if upper_input == i {
+	else if input == "I" {
 		true
 	}
-	else if upper_input == h {
+	else if input == "H" {
 		true
 	}
-	else if upper_input == d {
+	else if input == "D" {
 		true
 	}
 	else {
@@ -94,58 +83,93 @@ fn validate_user_input(input:&str) -> bool {
 	}
 }
 /*
-* execute_command(command:& String, house:& [Room;6], player:&mut Player)
+* execute_command(command:& str, house:& [Room;6], player:&mut Player, index:&mut usize)
 * Routes the command to the proper method that will execute the command.
 * Returns -1 if lose condition has been met.
 * Returns 0 if neither win nor lose condition met.
 * Returns 1 if win condition has been met.
 */
 fn execute_command(command:&str, house:& [Room;6], player:&mut Player, index:&mut usize) -> i32{
-	println!("We made it!");
-	let n = "N".to_owned();
-	let s = "S".to_string();
-	let l = "L".to_string();
-	let i = "I".to_string();
-	let h = "H".to_string();
-	let d = "D".to_string();
-	if command == n {
-		println!("We Really made it!");
-		*index=*index+1;
-		println!("index is now {}",index);
+
+	//if command is for north door.
+	if command == "N" {
+		if house[*index].get_north_door(){
+			*index=*index+1;
+		}else{
+			println!("There is no North door!");
+		}
 		0
-	}
-	else if command == s {
+	}//if command is for south door.
+	else if command == "S" {
+		if house[*index].get_south_door(){
+			*index=*index-1;
+		}else{
+			println!("There is no South door!");
+		}
 		0
-	}
-	else if command == l {
+	}//if commmand is for look.
+	else if command == "L" {
+		/*Condition for cream.*/
+		if house[*index].get_item() == 1 {
+			player.get_cream();
+			println!("You found some creamy Cream!");
+		}/*Condition for coffee.*/
+		else if house[*index].get_item() == 2 {
+			player.get_coffee();
+			println!("You found some hot Coffee!");
+		}/*Condition for sugar.*/
+		else if house[*index].get_item() == 3{
+			player.get_sugar();
+			println!("You found some sweet Sugar!");
+		}/*Condition for 0 as a return.*/
+		else{
+			println!("Nothing was found here!");
+		}
 		0
-	}
-	else if command == i {
+	}//if command is for inventory.
+	else if command == "I" {
+		let inventory = player.show_inventory();
+		println!("{}",inventory );
 		0
-	}
-	else if command == h {
+	}//if command is for help.
+	else if command == "H" {
+		println!("Commands explained:");
+		println!("N: Go through North door if available.");
+		println!("S: Go through South door if available.");
+		println!("L: Look for one of the three items required to win.");
+		println!("H: Displays an explanation for each command.");
+		println!("I: Show current inventory");
+		println!("D: Game ender. Will you taste sweet victory? Or taste bitter defeat? WARNING! You will lose if you have not found all three items before using this command and the game will end!");
 		0
-	}
-	else if command == d {
-		0
-	}
+	}//if command is for drink
+	else if command == "D" {
+		//Get the drink message.
+		let drink = player.drink();
+		println!("{}",drink );
+		//Check if the player has won.
+		if player.can_win() {
+		    return 1;
+		}else{
+			return -1;
+		}
+	}//default
 	else {
 		0
 	}
 }
 
-//End main.rs //
+//End CoffeeMaker functions //
 
 /*
 * Tests Below
 * Order of test blocks:
 *	1.) validate_input_tests
 *	2.) execute_command_tests
-*	3.)
 */
 
+ // Start of the validate_input_tests //
+
 /*
-* Start of the validate_input_tests
 * 1.) Tests each valid possible input for validate_user_input() and tests potential edge cases.
 */
 #[test]//Tests lowercase n as input.
@@ -284,9 +308,9 @@ fn validate_user_input_test18(){
 
 // End of validate_user_input tests. //
 
+// Start of execute_command_tests //
 
 /*
-* Start of the validate_input_tests
 * 2.) Tests each valid possible input for execute_command() and tests potential edge cases.
 */
 
@@ -357,5 +381,5 @@ fn execute_command_test6(){
 		assert!(false);
 	}
 }
-
 // End of execute_command_tests //
+// End of Test Blocks //
