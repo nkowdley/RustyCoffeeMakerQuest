6b123a27cef1a96b2c3c0f11b1c7d5f3681d6539
dms163 <dms163@pitt.edu>
2016-04-18 21:33:31 -0400

    Added new tests and refactored current tests to pass. Also added functionality to validate_user_input

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,12 @@ fn main() {
 		let mut input:String = get_user_input();
 		//if prompt_user_input:
 		while !validate_user_input(input){
+			println!("Invalid Command!");
 			input = get_user_input();
 		}
 
-
 		//do things
+
 	}
 
 }
@@ -35,66 +36,144 @@ fn main() {
 fn get_user_input() -> String {
 	println!("Please enter a command!");
 	let mut input = String::new();
-	try!(io::stdin().read_line(&mut input));
-	let mut input_trimmed = input.trim();
-    Ok(input_trimmed)
+    let stdin = io::stdin();
+    stdin.lock().read_line(&mut input).unwrap();
+    input
 }
 
 fn validate_user_input(input:String) -> bool {
-	false
+	let mut upper_input = String::new();
+	let N = "N".to_string();
+	let S = "S".to_string();
+	let L = "L".to_string();
+	let I = "I".to_string();
+	let H = "H".to_string();
+	let D = "D".to_string();
+	upper_input = input.to_uppercase();
+	upper_input = upper_input.to_string();
+	if upper_input == N {
+		return true;
+	}
+	else if upper_input == S {
+		return true;
+	}
+	else if upper_input == L {
+		return true;
+	}
+	else if upper_input == I {
+		return true;
+	}
+	else if upper_input == H {
+		return true;
+	}
+	else if upper_input == D {
+		return true;
+	}
+	else{
+		return false;
+	}
 }
+//End main
 
-#[test] //Tests each possible input.
+/*
+Tests Below
+Order of tests goes as follows:
+	1.) validate_input_tests
+	2.)
+
+*/
+
+// 1.) Tests each valid possible input for validate_user_input and tests potential edge cases.
+
+#[test] //Tests lowercase n as input.
 fn validate_user_input_test1() {
 	let test_string = "n".to_string();
-	let validate_result = validate_user_input(test_string);
-	assert!(true, validate_result);
+	let validate_result:bool = validate_user_input(test_string);
+	assert!(validate_result);
 }
 
-#[test]//Tests S as input
+#[test]//Tests lowercase s as input
 fn validate_user_input_test2(){
 	let test_string = "s".to_string();
 	let validate_result = validate_user_input(test_string);
-	assert!(true, validate_result);
+	assert!(validate_result);
 
 }
 
-#[test] //Tests L for input
+#[test] //Tests lowercase l as input
 fn validate_user_input_test3(){
 	let test_string = "l".to_string();
 	let validate_result = validate_user_input(test_string);
-	assert!(true, validate_result);
+	assert!(validate_result);
 }
 
-#[test] //Tests I for input
+#[test] //Tests lowercase i as input
 fn validate_user_input_test4(){
 	let test_string = "i".to_string();
 	let validate_result = validate_user_input(test_string);
-	assert!(true, validate_result);
+	assert!(validate_result);
 }
 
-#[test] //Tests H for input
+#[test] //Tests H as input
 fn validate_user_input_test5(){
 	let test_string = "h".to_string();
 	let validate_result = validate_user_input(test_string);
-	assert!(true, validate_result);
+	assert!(validate_result);
 }
 
-#[test] //Tests for D for input
+#[test] //Tests for lowercase d as input
 fn validate_user_input_test6(){
 	let test_string = "d".to_string();
 	let validate_result = validate_user_input(test_string);
-	assert!(true, validate_result);
+	assert!(validate_result);
 }
 #[test] //Tests for a string of numbers
 fn validate_user_input_test7(){
 	let test_string = "01234567891011121314151617181920".to_string();
 	let validate_result = validate_user_input(test_string);
-	assert!(false, validate_result);	
+	if validate_result == false{
+		assert!(true);
+	}else{
+		assert!(validate_result);
+	}	
 }
 #[test] //Tests for a string of mixed cases/characters and also will serve as a test for input other than standard commands.
 fn validate_user_input_test8(){
 	let test_string = "$HelloWorld!@".to_string();
 	let validate_result = validate_user_input(test_string);
-	assert!(false, validate_result);	
+	if validate_result == false{
+		assert!(true);
+	}else{
+		assert!(validate_result);
+	}	
+}
+#[test]//Tests for a valid command with an invalid character.
+fn validate_user_input_test9(){
+	let test_string = "N@".to_string();
+	let validate_result = validate_user_input(test_string);
+	assert!(false);	
+}
+#[test]//Tests for empty string being passed in as input.
+fn validate_user_input_test10(){
+	let test_string = " ".to_string();
+	let validate_result = validate_user_input(test_string);
+	assert!(false);	
+}
+#[test]//Tests for the ascii value of a valid input.
+fn validate_user_input_test11(){
+	let test_string = "78".to_string();
+	let validate_result = validate_user_input(test_string);
+	assert!(false);	
+}
+#[test]//Tests for a string of pure punctuation. 
+fn validate_user_input_test12(){
+	let test_string = " !.,"";?': ".to_string();
+	let validate_result = validate_user_input(test_string);
+	assert!(false);	
+}
+#[test]//Tests for uppercase N
+fn validate_user_input_test13(){
+	let test_string = "N".to_string();
+	let validate_result = validate_user_input(test_string);
+	assert!(false);			
 }
